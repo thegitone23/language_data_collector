8485d8c208e720966ce386add48ce73a6a76bb57
Mayank Gautam <mayankgautam98@gmail.com>
2020-12-18 21:37:04 +0530

    added basic functionality to register and logout

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_user
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user
 from app.models import User
 
 @app.route("/")
@@ -9,7 +9,7 @@ from app.models import User
 def index():
   return render_template("index.html", announcements = ["Work Under Progress"] )
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
   if current_user.is_authenticated:
     return redirect(url_for("index"))
@@ -24,3 +24,22 @@ def login():
     return redirect(url_for("index"))
 
   return render_template("login.html", form=form)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for("index"))
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash("Congratulations, you are now a registered user!")
+    return redirect(url_for("login"))
+  return render_template("register.html", title="Register", form=form)
+
+@app.route("/logout")
+def logout():
+  logout_user()
+  return redirect(url_for("index"))
