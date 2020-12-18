24d6995a2ca9d18a81766c15b0a98fd06b74f4a9
Mayank Gautam <mayankgautam98@gmail.com>
2020-12-18 17:01:18 +0530

    resolved cicular dependency

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from . import routes, models
-
-login = LoginManager(app)
