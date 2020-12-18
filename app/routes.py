31549a5b8fec780ec3e2135346593009bfdef82d
Mayank Gautam <mayankgautam98@gmail.com>
2020-12-18 08:15:06 +0530

    added login form template and the corresponding view

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
 def index():
   return render_template("index.html", announcements = ["Work Under Progress"] )
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    flash("User {} with remember me={} .. Loging In".format(form.username.data, form.remember_me.data))
+    return redirect('/')
+  return render_template("login.html", form=form)
