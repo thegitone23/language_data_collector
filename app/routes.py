86290d196ebb81b97af293982872cc28142f7248
Mayank Gautam <mayankgautam98@gmail.com>
2020-12-18 17:02:50 +0530

    added login functionality to login view

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-
+from flask_login import current_user, login_user
+from app.models import User
 
 @app.route("/")
 @app.route("/index")
@@ -10,8 +11,16 @@ def index():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+  if current_user.is_authenticated:
+    return redirect(url_for("index"))
+
   form = LoginForm()
   if form.validate_on_submit():
-    flash("User {} with remember me={} .. Loging In".format(form.username.data, form.remember_me.data))
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+      flash("Invalid Credentials")
+      return redirect(url_for("login"))
+    login_user(user, remember=form.remember_me.data) 
     return redirect(url_for("index"))
+
   return render_template("login.html", form=form)
