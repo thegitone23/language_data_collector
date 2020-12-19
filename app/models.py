4ed496510207dde0af21c94eadca03dabdb28d6b
Mayank Gautam <mayankgautam98@gmail.com>
2020-12-19 07:11:03 +0530

    added Sentence and Reply models and migrated the db

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
+  replies = db.relationship('Reply', backref='author', lazy='dynamic')
 
   def set_password(self, password):
     self.password_hash = generate_password_hash(password)
@@ -15,6 +17,24 @@ class User(UserMixin, db.Model):
 
   def __repr__(self):
     return "<User {}>".format(self.username)
+
+class Sentence(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  text = db.Column(db.Unicode(192))
+  reply_count = db.Column(db.Integer,default=0, index=True)
+  replies = db.relationship('Reply', backref='sentence', lazy='dynamic')  
+
+  def __repr__(self):
+    return "<Sentence : {}>".format(self.text)
+
+class Reply(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  text = db.Column(db.Unicode(192))
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  sentence_id = db.Column(db.Integer, db.ForeignKey('sentence.id'))   
+
+  def __repr__(self):
+    return "<Reply : {}>".format(self.text)
 
 @login.user_loader
 def load_user(id):
